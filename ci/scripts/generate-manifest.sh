906f48ebe0ee529bfceef31605a6f30867f2ae04
Parag Doshi <doshiparag@gmail.com>
2018-11-13 18:25:47 -0500

    removed services section.

--- ci/scripts/generate-manifest.sh
+++ ci/scripts/generate-manifest.sh
@@ -8,7 +8,6 @@ cp ./git-assets/ci/templates/manifest-template.yml ./manifest-output/manifest.ym
 # Replace variable names (using % as deliminator to avoid having to mess with / path variable)
 sed -i "s%APPNAME%$CF_APP-$(cat ./app-info/next-deployment.txt)%" ./manifest-output/manifest.yml
 sed -i "s%APPPATH%$APP_PACKAGE%" ./manifest-output/manifest.yml
-sed -i "s%APPSERVICES%$(cat ./app-info/services.txt)%" ./manifest-output/manifest.yml
 sed -i "s%APPSCALE%$(cat ./app-info/scale.txt)%" ./manifest-output/manifest.yml
 
 # Bit of debugging
